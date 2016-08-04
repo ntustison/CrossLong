d73202bbf7f242d512e642d8f7ca7285b6ba02a8
Nick Tustison <ntustison@gmail.com>
2016-08-03 22:25:50 -0700

    ENH:  Reorganize files and create 3-D figures.

---
+++ Template/createPngs.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/sh
+
+# unnormed
+
+antsVol -i sum.nii.gz -d unnormed_posterior3d_90x90x0.png[1.5,90x90x0,255x255x255] -f T_template0UnnormedRatios.mha
+antsVol -i sum.nii.gz -d unnormed_superior3d_90x180x0.png[1.5,90x180x0,255x255x255] -f T_template0UnnormedRatios.mha
+antsVol -i sum.nii.gz -d unnormed_right3d_180x0x90.png[1.5,180x0x90,255x255x255] -f T_template0UnnormedRatios.mha
+antsVol -i sum.nii.gz -d unnormed_left3d_0x0x270.png[1.5,0x0x270,255x255x255] -f T_template0UnnormedRatios.mha
+
+
+# normed
+
+antsVol -i sum.nii.gz -d normed_posterior3d_90x90x0.png[1.5,90x90x0,255x255x255] -f T_template0NormedRatios.mha
+antsVol -i sum.nii.gz -d normed_superior3d_90x180x0.png[1.5,90x180x0,255x255x255] -f T_template0NormedRatios.mha
+antsVol -i sum.nii.gz -d normed_right3d_180x0x90.png[1.5,180x0x90,255x255x255] -f T_template0NormedRatios.mha
+antsVol -i sum.nii.gz -d normed_left3d_0x0x270.png[1.5,0x0x270,255x255x255] -f T_template0NormedRatios.mha
