645ac1d8775f8e6f103494f2567e97c2032ed274
Nick Tustison <ntustison@gmail.com>
2016-10-28 08:53:43 -0700

    ENH:  More figures.

---
+++ data/plot_median_ratios.R
@@ -0,0 +1,11 @@
+library( ggplot2 )
+
+ratios <- read.csv( "median_ratios.csv" )
+
+ratiosPlot <- ggplot( data = ratios ) +
+              geom_point( aes( y = Ratios, x = Regions, colour = Pipeline, shape = Pipeline ), size = 3 ) +
+              theme( axis.text.x = element_text( face="bold", size = 8, angle = 60, hjust = 1 ) ) +
+              scale_color_manual( values = colorRampPalette( c( "navyblue", "darkred" ) )(3) ) +
+              labs( x = 'Cortical Region', y = 'Ratio Value' ) +
+              theme( legend.position = "right" )
+ggsave( "~/Desktop/ratios.pdf", ratiosPlot, width = 10, height = 3 )
