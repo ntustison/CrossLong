bdfee15f75db19569cbf4f00ee8e960f4a346475
Nick Tustison <ntustison@gmail.com>
2019-01-09 19:57:30 -1000

    ENH:  Prepare for HBM submission.

---
+++ Manuscript/NeuroImage/stitchManuscript.R
@@ -0,0 +1,47 @@
+library( rmarkdown )
+library( ggplot2 )
+
+stitchedFile <- "stitched.Rmd";
+
+formatFile <- "format.Rmd"
+
+rmdFiles <- c( formatFile,
+               "titlePage.Rmd",
+#               "notes.Rmd",
+               "abstract.Rmd",
+               "intro.Rmd",
+               "imagingMethods.Rmd",
+               "processingMethods.Rmd",
+               "statisticalMethods2.Rmd",
+               "results.Rmd",
+               "discussion.Rmd"
+   )
+
+
+rmdFiles <- c( formatFile,
+   "titlePage.Rmd",
+  #               "notes.Rmd",
+   "abstract.Rmd",
+   "intro.Rmd",
+   "imagingMethods.Rmd",
+   "processingMethods.Rmd",
+   "statisticalMethods2.Rmd",
+   "results.Rmd",
+   "discussion.Rmd",
+   "appendix.Rmd",
+   "acknowledgments.Rmd"
+   )
+
+for( i in 1:length( rmdFiles ) )
+  {
+  if( i == 1 )
+    {
+    cmd <- paste( "cat", rmdFiles[i], ">", stitchedFile )
+    } else {
+    cmd <- paste( "cat", rmdFiles[i], ">>", stitchedFile )
+    }
+  system( cmd )
+  }
+
+cat( '\n Pandoc rendering', stitchedFile, '\n' )
+render( stitchedFile, output_format = "pdf_document" )
