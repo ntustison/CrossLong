54ab0ff9ba72c89de6ee3cbc23394eb587375828
Nick Tustison <ntustison@gmail.com>
2019-05-27 09:13:50 -0700

    ENH:  Revision.

--- Manuscript/JAD/stitchManuscript.R
+++ Manuscript/JAD/stitchManuscript.R
@@ -6,19 +6,6 @@ stitchedFile <- "stitched.Rmd";
 formatFile <- "format.Rmd"
 
 rmdFiles <- c( formatFile,
-               "titlePage.Rmd",
-#               "notes.Rmd",
-               "abstract.Rmd",
-               "intro.Rmd",
-               "imagingMethods.Rmd",
-               "processingMethods.Rmd",
-               "statisticalMethods2.Rmd",
-               "results.Rmd",
-               "discussion.Rmd"
-   )
-
-
-rmdFiles <- c( formatFile,
    "titlePage.Rmd",
   #               "notes.Rmd",
    "abstract.Rmd",
@@ -44,4 +31,29 @@ for( i in 1:length( rmdFiles ) )
   }
 
 cat( '\n Pandoc rendering', stitchedFile, '\n' )
-render( stitchedFile, output_format = c( "pdf_document", "word_document" ) )
+render( stitchedFile, output_format = c( "pdf_document" ) )
+
+
+
+stitchedFile <- "stitched2.Rmd";
+
+formatFile <- "format.Rmd"
+
+rmdFiles <- c( formatFile,
+               "floats.Rmd"
+             )
+
+for( i in 1:length( rmdFiles ) )
+  {
+  if( i == 1 )
+    {
+    cmd <- paste( "cat", rmdFiles[i], ">", stitchedFile )
+    } else {
+    cmd <- paste( "cat", rmdFiles[i], ">>", stitchedFile )
+    }
+  system( cmd )
+  }
+
+cat( '\n Pandoc rendering', stitchedFile, '\n' )
+render( stitchedFile, output_format = c( "pdf_document" ) )
+
